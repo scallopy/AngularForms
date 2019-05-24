425b8b247b3b0a0363bcde01a3bfc3c105f8e9aa
scallopy <scallopy@abv.bg>
2019-05-24 16:20:41 +0300

    Set Default Values with ngModel Property Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
+  defaultQuestion = 'pet';
 
   suggestedUserName() {
     const suggestedUserName = 'Superuser';
