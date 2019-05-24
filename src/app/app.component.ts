7191fb7a9927b9e4e8742756e2939ecde3704083
scallopy <scallopy@abv.bg>
2019-05-24 13:10:01 +0300

    Template-Driven

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   suggestedUserName() {
     const suggestedUserName = 'Superuser';
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form);
   }
 }
